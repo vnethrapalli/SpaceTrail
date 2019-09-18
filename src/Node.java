965708c6bd0a63c49b6346f0c9422e5f14a48cae
vigne <vignet009@gmail.com>
2019-09-18 09:22:19 -0400

    blank title screen

--- src/Node.java
+++ src/Node.java
@@ -9,6 +9,7 @@ public class Node {
     private String text;
     private String[] answers;
     private String file;
+
     public Node(String file) {
         this.file=file;
         try {
@@ -16,6 +17,14 @@ public class Node {
         }catch (FileNotFoundException ex){
             System.out.println("oops");
         }
+
+        JFrame branch = new JFrame();
+        branch.setSize(2 * (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth() / 3 , 4 * (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight() / 5);
+        //https://stackoverflow.com/questions/9706097/panel-size-changes-from-computer-to-computer
+        branch.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        branch.setLocationRelativeTo(null);
+        branch.add(new NodePanel());
+
     }
     private void loadfile(String file) throws FileNotFoundException {
         BufferedReader in = new BufferedReader(new FileReader(file));
@@ -23,7 +32,8 @@ public class Node {
     private class NodePanel extends JPanel {
 
         protected void paintComponent(Graphics g) {
-
+            g.setColor(Color.blue);
+            g.fillRect(0, 0, getWidth(), getHeight());
         }
     }
 }
