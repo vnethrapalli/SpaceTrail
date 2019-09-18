ab0d61adbc4782b5313da465730e8f7e63e1e912
Max-Bus <34168923+Max-Bus@users.noreply.github.com>
2019-09-18 09:42:25 -0400

    gi

--- src/Node.java
+++ src/Node.java
@@ -3,18 +3,19 @@ import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Node {
-
+    public final int ROW_LENGTH=4;
     private String text;
-    private String[] answers;
+    private String[][] answers;
     private String file;
 
     public Node(String file) {
         this.file=file;
         try {
             loadfile(file);
-        }catch (FileNotFoundException ex){
+        }catch (IOException ex){
             System.out.println("oops");
         }
 
@@ -26,9 +27,21 @@ public class Node {
         branch.add(new NodePanel());
 
     }
-    private void loadfile(String file) throws FileNotFoundException {
+    private void loadfile(String file) throws IOException {
         //https://www.mkyong.com/java/how-to-read-file-from-java-bufferedreader-example/
         BufferedReader in = new BufferedReader(new FileReader(file));
+        this.text=in.readLine();
+        String num=in.readLine();
+        int number=Integer.parseInt(num);
+        answers=new String[number][ROW_LENGTH];
+        for (int i = 0; i < number; i++) {
+            String line = in.readLine();
+            String[] parts=line.split(" ");
+            for (int j = 0; j <ROW_LENGTH ; j++) {
+                answers[i][j]=parts[j];
+            }
+        }
+
     }
     private class NodePanel extends JPanel {
 
---
+++ src/tester.java
@@ -0,0 +1,5 @@
+public class tester {
+    public static void main(String[] args) {
+        //Node n = new Node("test.txt");
+    }
+}
