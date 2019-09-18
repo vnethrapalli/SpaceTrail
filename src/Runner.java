8bb30b976f8500b595bf44d8d68a6cd63ad609f0
Max-Bus <34168923+Max-Bus@users.noreply.github.com>
2019-09-18 09:59:32 -0400

    gi

--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Runner {
-
 
     public static void main(String[] args) {
 
@@ -15,7 +18,26 @@ public class Runner {
         screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         screen.setLocationRelativeTo(null);
         screen.setVisible(true);
+        save(new Node("test.txt"));
+        try {
+            System.out.println(load().FILE);
+        }catch (Exception e){
+            System.out.println("cant load");
+        }
 
+    }
+    public static void save(Node curnode){
+        //https://www.javatpoint.com/java-filewriter-class
+        try{
+            FileWriter fw=new FileWriter("C:\\Users\\NerdMachine2\\IdeaProjects\\SpaaceTrail\\savefile");
+            fw.write(curnode.FILE);
+            fw.close();
+        }catch(Exception e){System.out.println(e);}
+        System.out.println("Success...");
+    }
+    public static Node load() throws Exception{
+            BufferedReader in = new BufferedReader(new FileReader("savefile"));
+            return new Node(in.readLine());
 
     }
 }
