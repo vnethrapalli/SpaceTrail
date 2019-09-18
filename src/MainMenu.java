06eac31b7c2afd421095da6861e827df7ba7ae28
vigne <vignet009@gmail.com>
2019-09-18 10:09:32 -0400

    blank title screen

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -5,14 +5,17 @@ import java.awt.event.MouseEvent;
 
 public class MainMenu extends JPanel {
 
-    private Font title = new Font("Matura MT Script Capitals", Font.BOLD,
-            (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight() / 10);
-
-    private FontMetrics f_title = getFontMetrics(title);
+    private Font title;
+    private FontMetrics f_title;
 
     private int mouse_x, mouse_y;
+    int w = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(),
+            h = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
 
     public MainMenu() {
+        title = new Font("Matura MT Script Capitals", Font.BOLD,h / 10);
+        f_title =  getFontMetrics(title);
+
         addMouseMotionListener(new MouseAdapter() {
             @Override
             public void mouseMoved(MouseEvent e) {
@@ -28,11 +31,18 @@ public class MainMenu extends JPanel {
 
     protected void paintComponent(Graphics g) {
 
+        //background
         g.setColor(Color.black);
         g.fillRect(0, 0, getWidth(), getHeight());
 
+        //title
         g.setColor(Color.yellow);
         g.setFont(title);
         g.drawString("Space Trail", (getWidth()  - f_title.stringWidth("Space Trail")) / 2, getHeight() / 3);
+
+        //button
+        //g.fillRect();
+
+
     }
 }
