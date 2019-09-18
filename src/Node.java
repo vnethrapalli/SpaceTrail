2a630289a391760762dfceaed224f03b27b927b6
Max-Bus <34168923+Max-Bus@users.noreply.github.com>
2019-09-18 09:18:44 -0400

    gi

--- src/Node.java
+++ src/Node.java
@@ -1,14 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
 
 public class Node {
 
     private String text;
-
-    public Node(String text) {
-        this.text = text;
+    private String[] answers;
+    private String file;
+    public Node(String file) {
+        this.file=file;
+        loadfile(file);
     }
-
+    private void loadfile(String file) {
+        BufferedReader in = new BufferedReader();
+    }
     private class NodePanel extends JPanel {
 
         protected void paintComponent(Graphics g) {
