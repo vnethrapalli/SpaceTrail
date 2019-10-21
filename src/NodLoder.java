7d2de100d28119fc8602a90d65efb09f62e2d55d
adruz <adruzenko03@gmail.com>
2019-10-21 08:08:30 -0400

    added more to shop, comments

---
+++ src/NodLoder.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+
+public class NodLoder extends JPanel {
+    private Font title;
+    private FontMetrics f_title;
+
+    private int mouse_x, mouse_y;
+
+    public NodLoder(String filename) {
+        Font customFont=null;
+        try {
+            //create the font to use. Specify the size!
+            //https://www.dafontfree.net/freefonts-matura-mt-script-capitals-f65093.htm
+            //https://stackoverflow.com/questions/5652344/how-can-i-use-a-custom-font-in-java
+            customFont = Font.createFont(Font.TRUETYPE_FONT, new File("MATURASC.ttf")).deriveFont(Font.BOLD,(int)Toolkit.getDefaultToolkit().getScreenSize().getHeight()/10);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch(FontFormatException e) {
+            e.printStackTrace();
+
+        }
+        //create the font
+
+        //title = new Font("Matura MT Script Capitals", Font.BOLD,h / 10);
+
+        title = customFont;
+        f_title =  getFontMetrics(title);
+
+        addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+                mouse_x = e.getX();
+                mouse_y = e.getY();
+
+                repaint();
+
+            }
+
+        });
+    }
+    protected void paintComponent(Graphics g) {
+
+        //background
+        g.setColor(Color.black);
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        //title
+        g.setColor(Color.yellow);
+        g.setFont(title);
+
+        //button
+        g.fillRect(2 * getWidth() / 5, getHeight() / 2, getWidth() / 5, getHeight() / 20);
+
+
+    }
+
+}
